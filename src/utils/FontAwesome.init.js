d5783debf9291c4681356583c436dced8bca6d04
Joan Maria Talarn <joan.talarn@esl-education.org>
2018-11-13 11:02:27 +0100

    Initialization FontAwesome

---
+++ src/utils/FontAwesome.init.js
@@ -0,0 +1,9 @@
+import { library } from '@fortawesome/fontawesome-svg-core';
+
+import {
+	faSearch, faStepBackward, faStepForward, faBox
+} from '@fortawesome/free-solid-svg-icons';
+// import { faStar as faStarEmpty } from '@fortawesome/free-regular-svg-icons';
+// import { faProductHunt } from '@fortawesome/free-brands-svg-icons';
+
+library.add(faSearch, faStepBackward, faStepForward, faBox);
